90b30dc699bfc04f94dfe5f39762a58c83d7dd2d
suhaina2002 <110049755+suhaina2002@users.noreply.github.com>
2022-07-26 21:41:27 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,19 @@
+package week2.day1;
+
+public class swap {
+	public static void main(String[] args) {	
+	int a=20;
+	int b=10;
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+	
+	System.out.println("value of a:"+a);
+	System.out.println("value of b:"+b);
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
