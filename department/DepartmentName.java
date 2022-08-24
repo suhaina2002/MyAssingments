1484795091c6c5380bc97d36f1228bce3ab4476f
suhaina2002 <110049755+suhaina2002@users.noreply.github.com>
2022-08-24 22:01:00 +0530

    Add files via upload

---
+++ department/DepartmentName.java
@@ -0,0 +1,17 @@
+package org.department;
+
+public class DepartmentName {
+	
+	public void Department() {
+		System.out.println("Department");
+	}
+
+public static void main (String[]args) {
+ DepartmentName dep=new DepartmentName();
+ dep.Department();
+
+}
+
+	}
+
+
